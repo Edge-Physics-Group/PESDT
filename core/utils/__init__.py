51d038eedda019cdaefd71c254d66aee8032b799
V-.P. R. <v.rikala@outlook.com>
2025-06-17 15:51:03 +0300

    Getting rid of wildcard imports

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,5 +1,5 @@
-from .amread import *
-from .atomic import *
-from .machine_defs import *
+from .amread import reactions, read_amjuel_1d, read_amjuel_2d, calc_cross_sections, calc_photon_rate, A_coeff, wavelength, FF, pp, Bmn_coeff, photon_rate_coeffs
+from .atomic import stdchannel_redirected, get_ADAS_dict
+from .machine_defs import MachineDefs, get_DIIIDdefs, get_JETdefs, los_width_from_neigbh, rotate_los, poloidal_angle
 #from .utils import *
-from .utility_functions import *
+from .utility_functions import find_nearest, floatToBits, gaussian, interp_nearest_neighb, isclose
