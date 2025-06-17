c28f15e79dc17d3fa404af18e9ccba0147109222
V-.P. R. <v.rikala@outlook.com>
2025-06-17 16:04:33 +0300

    fix

--- cherab_bridge/__init__.py
+++ cherab_bridge/__init__.py
@@ -2,4 +2,4 @@ from .cherab_AMJUEL_data import AMJUEL_Data
 from .cherab_atomic_data import PESDT_ADAS_Data
 from .cherab_plasma import CherabPlasma
 from .continuo import continuo
-from .createCherabPlasma import createCherabPlasma, BaseD, D0, D2, D3
+from .createCherabPlasma import createCherabPlasma
