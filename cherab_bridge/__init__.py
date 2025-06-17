fef5190cfb06a1f46247c08b8495f830f94fd303
V-.P. R. <v.rikala@outlook.com>
2025-06-17 15:09:40 +0300

    Fixing import paths

--- cherab_bridge/__init__.py
+++ cherab_bridge/__init__.py
@@ -2,5 +2,4 @@ from cherab_AMJUEL_data import *
 from cherab_atomic_data import *
 from cherab_plasma import *
 from continuo import *
-from edge2d_cherab_bridge import *
-from solps_cherab_bridge import *
+from createCherabPlasma import *
