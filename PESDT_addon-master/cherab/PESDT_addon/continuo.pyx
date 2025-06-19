8ec039db1bd907ce34693c94cddc20a0029cc78c
V-.P. R. <v.rikala@outlook.com>
2025-06-19 22:10:26 +0300

    Maybe this works.

--- PESDT_addon-master/cherab/PESDT_addon/continuo.pyx
+++ PESDT_addon-master/cherab/PESDT_addon/continuo.pyx
@@ -11,8 +11,7 @@ from cherab.core.utility.constants cimport RECIP_4_PI
 cimport cython
 
 cdef extern from "/home/adas/include/adaslib.h":
-    cdef void continuo_(double *wave   , double *tev    , int *iz0,
-                      int *iz1       , double *contff , double *contin )
+    cdef void continuo_(double *wave, double *tev, int *iz0, int *iz1, double *contff, double *contin )
 
 import numpy as np
 
@@ -103,7 +102,7 @@ cdef class Continuo(PlasmaModel):
         wvl_A = wvl * 10.
         iz0=1
         iz1=1
-        continuo_(*wvl_A , *te , *iz0 , *iz1, *contin, *contff )
+        continuo_(*wvl_A , *te , *iz0 , *iz1, *contff, *contin)
 
         return RECIP_4_PI*contin*(1e-6)*ne*ne*10
 
