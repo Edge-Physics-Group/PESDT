1a48aee1981ba35eaf74a3168ee4f832834a5261
V-.P. R. <v.rikala@outlook.com>
2025-06-23 15:33:30 +0300

    try .strip()

--- core/utils/yacoraread.py
+++ core/utils/yacoraread.py
@@ -12,8 +12,8 @@ class YACORA():
         print(repr(h2_data_path))
         print(type(h_data_path))
         print(repr(h_data_path))
-        h_data, _ = self.read_yacora_rate(h_data_path)
-        h2_data,_ = self.read_yacora_rate(h2_data_path)
+        h_data, _ = self.read_yacora_rate(h_data_path.strip())
+        h2_data,_ = self.read_yacora_rate(h2_data_path.strip())
         self.h_rates = {3: h_data}
         self.h2_rates = {3: h2_data}
 
