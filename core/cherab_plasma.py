535a19649b4aa693fc217300e462a714927924be
V-.P. R. <v.rikala@outlook.com>
2025-06-23 16:31:01 +0300

    typo

--- core/cherab_plasma.py
+++ core/cherab_plasma.py
@@ -181,7 +181,7 @@ class CherabPlasma():
                     h_line = PESDTLine(D0, 0, transition)
                     model_list.append(DirectEmission(h_line, lineshape=lineshape))
                 if include_H2:
-                    h_line = PESDTLine(D2, 1, transition) # Increment charge by one 
+                    h_line = PESDTLine(D2, 0, transition) # Increment charge by one 
                     model_list.append(DirectEmission(h_line, lineshape=lineshape))
                 if include_ff_fb:
                     h_line = PESDTLine(D0, 0, transition)
