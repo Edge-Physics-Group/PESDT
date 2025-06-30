0964214244db550f66078c8dac2f9e42748b2d63
V-.P. R. <v.rikala@outlook.com>
2025-06-30 14:47:16 +0300

    Add new classes to init

--- PESDT_addon-master/cherab/PESDT_addon/__init__.py
+++ PESDT_addon-master/cherab/PESDT_addon/__init__.py
@@ -1,6 +1,6 @@
 from .continuo import Continuo
-from .Line import PESDTLine
-from .LineEmitters import DirectEmission, LineExcitation_AM, LineRecombination_AM, LineH2_AM, LineH2_pos_AM, LineH_neg_AM, LineH3_pos_AM
+from .Line import PESDTLine, PESDTLineMol
+from .LineEmitters import DirectEmission, DirectEmissionMol, LineExcitation_AM, LineRecombination_AM, LineH2_AM, LineH2_pos_AM, LineH_neg_AM, LineH3_pos_AM
 from .Maxwellian import PESDTMaxwellian
 from .PESDT_plasma import PESDTSimulation
 from .RateFunctions import RateFunction, NullRateFunction
