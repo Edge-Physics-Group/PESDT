526b225e0be9f5b9d8da8e750c3024560d0ebeab
V-.P. R. <v.rikala@outlook.com>
2025-07-01 14:23:18 +0300

    Fix stark line init call

--- PESDT_addon-master/cherab/PESDT_addon/stark.pyx
+++ PESDT_addon-master/cherab/PESDT_addon/stark.pyx
@@ -106,7 +106,7 @@ cdef class StarkBroadenedLine(LineShapeModel):
         double _fwhm_poly_coeff_lorentz[7]
         double _weight_poly_coeff[6]
 
-    def __init__(self, Line line, double wavelength, Species target_species, Plasma plasma,
+    def __init__(self, Line line, double wavelength, Species target_species, Plasma plasma, AtomicData atomic_data,
                  dict stark_model_coefficients=None, integrator=GaussianQuadrature()):
 
         stark_model_coefficients = stark_model_coefficients or self.STARK_MODEL_COEFFICIENTS_DEFAULT
