9d30344744d98f7f9996b5dd25668bc180281c00
V-.P. R. <v.rikala@outlook.com>
2025-07-17 10:14:23 +0300

    Fix typo in path

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -7,12 +7,12 @@ class spectroscopic_lines_db:
     '''
     def __init__(self):
         
-        self.H_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/H.json"))
-        self.He_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/He.json"))
-        self.C_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/C.json"))
-        self.Be_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/Be.json"))
-        self.N_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/N.json"))
-        self.W_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/W.json"))
+        self.H_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/H.json"))
+        self.He_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/He.json"))
+        self.C_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/C.json"))
+        self.Be_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/Be.json"))
+        self.N_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/N.json"))
+        self.W_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/W.json"))
 
     def _load(self, path):
         with open(path, "r") as f:
