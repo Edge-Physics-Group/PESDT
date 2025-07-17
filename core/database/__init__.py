4d706bbbe689b6c03a57c8121f0ab57e28bacfe3
V-.P. R. <v.rikala@outlook.com>
2025-07-17 10:14:05 +0300

    Hardcode paths

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -6,12 +6,13 @@ class spectroscopic_lines_db:
     Database convinience class for loading stored spectroscopic lines from JSON files
     '''
     def __init__(self):
-        self.H_lines = self._load( "H.json")
-        self.He_lines = self._load("He.json")
-        self.C_lines = self._load("C.json")
-        self.Be_lines = self._load("Be.json")
-        self.N_lines = self._load("N.json")
-        self.W_lines = self._load("W.json")
+        
+        self.H_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/H.json"))
+        self.He_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/He.json"))
+        self.C_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/C.json"))
+        self.Be_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/Be.json"))
+        self.N_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/N.json"))
+        self.W_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/databese/W.json"))
 
     def _load(self, path):
         with open(path, "r") as f:
