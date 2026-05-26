548806d41697576b544a3a8ae4a38f7788cc29a5
V-.P. R. <v.rikala@outlook.com>
2026-05-26 14:09:23 +0300

    move python object to pyx

--- PESDT_addon-master/cherab/PESDT_addon/LineShapes.pyx
+++ PESDT_addon-master/cherab/PESDT_addon/LineShapes.pyx
@@ -266,7 +266,23 @@ cdef class StarkBroadenedLine(LineShapeModel):
         over the spectral bin. Default is `GaussianQuadrature()`.
 
     """
+    STARK_MODEL_COEFFICIENTS_DEFAULT = {
+        (3, 2): (3.71e-18, 0.7665, 0.064),
+        (4, 2): (8.425e-18, 0.7803, 0.050),
+        (5, 2): (1.31e-15, 0.6796, 0.030),
+        (6, 2): (3.954e-16, 0.7149, 0.028),
+        (7, 2): (6.258e-16, 0.712, 0.029),
+        (8, 2): (7.378e-16, 0.7159, 0.032),
+        (9, 2): (8.947e-16, 0.7177, 0.033),
+        (4, 3): (1.330e-16, 0.7449, 0.045),
+        (5, 3): (6.64e-16, 0.7356, 0.044),
+        (6, 3): (2.481e-15, 0.7118, 0.016),
+        (7, 3): (3.270e-15, 0.7137, 0.029),
+        (8, 3): (4.343e-15, 0.7133, 0.032),
+        (9, 3): (5.588e-15, 0.7165, 0.033),
 
+
+    }
 
 
     def __init__(self, Line line, double wavelength, Species target_species, Plasma plasma, AtomicData atomic_data,
