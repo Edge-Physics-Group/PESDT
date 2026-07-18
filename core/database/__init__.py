511aac5e3a6b2e7ef938fe2de3eeb7f75963d453
V-.P. R. <v.rikala@outlook.com>
2026-07-18 14:40:28 +0300

    fix paths

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -6,13 +6,13 @@ class spectroscopic_lines_db:
     Database convinience class for loading stored spectroscopic lines from JSON files
     '''
     def __init__(self):
-        
-        self.H_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/H.json"))
-        self.He_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/He.json"))
-        self.C_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/C.json"))
-        self.Be_lines   = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/Be.json"))
-        self.N_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/N.json"))
-        self.W_lines    = self._load(os.path.join(os.path.expanduser("~"), "PESDT/core/database/W.json"))
+        pesdt_home = os.environ.get('PESDT_HOME', os.path.expanduser('~') + "PESDT/")
+        self.H_lines    = self._load(os.path.join(pesdt_home, "core/database/H.json"))
+        self.He_lines   = self._load(os.path.join(pesdt_home, "core/database/He.json"))
+        self.C_lines    = self._load(os.path.join(pesdt_home, "core/database/C.json"))
+        self.Be_lines   = self._load(os.path.join(pesdt_home, "core/database/Be.json"))
+        self.N_lines    = self._load(os.path.join(pesdt_home, "core/database/N.json"))
+        self.W_lines    = self._load(os.path.join(pesdt_home, "core/database/W.json"))
 
     def _load(self, path):
         with open(path, "r") as f:
