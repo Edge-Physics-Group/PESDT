f927c85c17ab3d3873059086843face19eb365cf
V-.P. R. <v.rikala@outlook.com>
2026-07-18 13:39:20 +0300

    remove pure cython imports from py init

--- PESDT_addon-master/cherab/PESDT_addon/__init__.py
+++ PESDT_addon-master/cherab/PESDT_addon/__init__.py
@@ -6,9 +6,7 @@ from .Line import PESDTLine, PESDTLineMol
 from .LineEmitters import DirectEmission, DirectEmissionMol, OpaqueDeltaDirectEmission, OpaqueGaussianDirectEmission, LineExcitation_AM, LineRecombination_AM, LineH2_AM, LineH2_pos_AM, LineH_neg_AM, LineH3_pos_AM
 from .Maxwellian import PESDTMaxwellian
 from .PESDT_plasma import PESDTSimulation
-from .RateFunctions import RateFunction, NullRateFunction
 from .Species import PESDTElement, PESDTSpecies, deuterium, hydrogen
-from .RateFunctions import RateFunction, NullRateFunction
 #from .stark import StarkFunction, StarkBroadenedLine
 from .LineShapes import StarkFunction, StarkBroadenedLine, DeltaLine
 from .mesh_geometry import EIRENEMesh, QuadMesh
