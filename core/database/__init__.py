64119b81380dc547ce2975ab0fcab9042972ae7a
V-.P. R. <v.rikala@outlook.com>
2026-07-29 13:38:02 +0300

    Explicitly use H for D and T

--- core/database/__init__.py
+++ core/database/__init__.py
@@ -8,13 +8,15 @@ class spectroscopic_lines_db:
     def __init__(self):
         pesdt_home = os.environ.get('PESDT_HOME', os.path.expanduser('~') + "PESDT/")
         self.H_lines    = self._load(os.path.join(pesdt_home, "core/database/H.json"))
+        self.D_lines    = self._load(os.path.join(pesdt_home, "core/database/H.json"))
+        self.T_lines    = self._load(os.path.join(pesdt_home, "core/database/H.json"))
         self.He_lines   = self._load(os.path.join(pesdt_home, "core/database/He.json"))
         self.C_lines    = self._load(os.path.join(pesdt_home, "core/database/C.json"))
         self.Be_lines   = self._load(os.path.join(pesdt_home, "core/database/Be.json"))
         self.N_lines    = self._load(os.path.join(pesdt_home, "core/database/N.json"))
         self.W_lines    = self._load(os.path.join(pesdt_home, "core/database/W.json"))
 
-        self.data = {"H": self.H_lines, "He": self.He_lines, "C": self.C_lines, "Be": self.Be_lines, "N": self.N_lines, "W": self.W_lines}
+        self.data = {"H": self.H_lines, "D": self.H_lines,"T": self.H_lines,"He": self.He_lines, "C": self.C_lines, "Be": self.Be_lines, "N": self.N_lines, "W": self.W_lines}
         # flatten data
         for species, data in self.data.items():
             flat_data = {}
