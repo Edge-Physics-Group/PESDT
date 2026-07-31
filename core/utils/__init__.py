dfde35b779b378af2cd53fefad1b25f8627e4f03
V-.P. R. <v.rikala@outlook.com>
2026-07-31 10:38:45 +0300

    Import aug

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -1,6 +1,6 @@
 from .amread import reactions, read_amjuel_1d, read_amjuel_2d, calc_cross_sections, calc_photon_rate, A_coeff, wavelength, FF, pp, Bmn_coeff, photon_rate_coeffs, H2_wavelength, H2_reactions, calc_H2_band_emission, doppler_absorbance, cen_absorbance, ideal_absorbance 
 from .adasdata import ADF11, ADF15, populate_adas_db
-from .machine_defs import MachineDefs, get_DIIIDdefs, get_JETdefs, los_width_from_neigbh, rotate_los, poloidal_angle
+from .machine_defs import MachineDefs, get_DIIIDdefs, get_JETdefs, get_AUGdefs,los_width_from_neigbh, rotate_los, poloidal_angle
 from .yacoraread import  YACORA
 from .utility_functions import find_nearest, floatToBits, gaussian, interp_nearest_neighb, isclose
 from .JET_mesh_from_grid import create_toroidal_wall_from_points, modify_wall_polygon_for_observer,plot_wall_modification
