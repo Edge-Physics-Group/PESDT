9bd58014571b063ef512a9e78c632a31ac207ffc
V-.P. R. <v.rikala@outlook.com>
2026-07-31 16:32:49 +0300

    Debug prints

--- core/cherab/createCherabPlasma.py
+++ core/cherab/createCherabPlasma.py
@@ -117,7 +117,8 @@ def createZCherabPlasma(PESDT, species_transitions: dict):
         else:
             n_exc = n_izs[bc_idx + charge -1, :]
             n_rec = n_izs[bc_idx + charge, :]
-
+        print(n_exc.shape)
+        print(n_rec.shape)
         for wl, transition in wl_transitions.items():
             emission_dict[exc_sp][transition] = adf.interpolate(te, ne, "EXCIT", wl)*ne*n_exc*1/(4.0*np.pi)
             emission_dict[rec_sp][transition] = adf.interpolate(te, ne, "RECOM", wl)*ne*n_rec*1/(4.0*np.pi)
