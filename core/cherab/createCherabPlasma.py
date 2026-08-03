74d7ed78e087e7f9bb2cee898f5e4b300964a663
V-.P. R. <v.rikala@outlook.com>
2026-08-03 09:54:47 +0300

    yacoraread returns only emission

--- core/cherab/createCherabPlasma.py
+++ core/cherab/createCherabPlasma.py
@@ -352,7 +352,7 @@ def createHydrogenicCherabPlasma(PESDT, transitions: dict,
             emission_keys +=mol_exc_bands
             for band in mol_exc_bands:
                 logger.info(f"   Band: {band}")
-                em, den = yacora.calc_H2_band_emission(te, ne, n2[:], band=band, tvib = 3000)
+                em = yacora.calc_H2_band_emission(te, ne, n2[:], band=band, tvib = 3000)
                 emission[6][band], species_density[6, :] = em, den
     else:
         #ADAS
